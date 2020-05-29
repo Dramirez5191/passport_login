aacb0509294519aa40af02ea2f857db35318c2e5
Dayan Ramirez <dayan@MacBook-Pro.local>
2020-05-29 19:00:18 -0400

    initial commit w/out flash msg

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
+require("dotenv").config();
+
 module.exports = {
-  MongoURI:
-    "mongodb+srv://admin:Abc123@cluster0-dbcm5.mongodb.net/test?retryWrites=true&w=majority",
+  MongoURI: process.env.MONGO_KEY,
 };
